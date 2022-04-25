926a240ac423f9a0d7f78c62669f5a32cbd237ab
Jadedragon3112 <ngoclong.work@gmail.com>
2022-04-25 18:14:57 +0700

    add validation form input CONTACT

--- w3_band/assets/js/main.js
+++ w3_band/assets/js/main.js
@@ -1,14 +1,14 @@
 // goi den button a
-var btnBuys = document.querySelectorAll('.js-button-buy-ticket')
+let btnBuys = document.querySelectorAll('.js-button-buy-ticket')
 
 //goi den container
-var getContainer = document.querySelector('.modal-container')
+let getContainer = document.querySelector('.modal-container')
 
 // lấy nút close
-var close_button = document.querySelector('.js-close-button')
+let close_button = document.querySelector('.js-close-button')
 
 // goi class modal
-var getModal = document.querySelector('.js-modal')
+let getModal = document.querySelector('.js-modal')
 
 // Tạo funtion thêm class open vào class modal
 function showContainer(){
@@ -18,9 +18,10 @@ function showContainer(){
 // ham remove khi nhan nut close
 function hideContainer(){
     getModal.classList.remove('open')
+    
 }
 // Ta0 vong lap bat su kien khi nhan nut
-for(var btnBuy of btnBuys){
+for(let btnBuy of btnBuys){
     btnBuy.addEventListener('click',showContainer )
 }
 
@@ -36,14 +37,14 @@ getContainer.addEventListener('click', function(e){
 })
 
 // get header responsive mobile
-var getHeader = document.querySelector('#header')
-var menuButton = document.querySelector('.menu-mobile')
+let getHeader = document.querySelector('#header')
+let menuButton = document.querySelector('.menu-mobile')
 // lấy giá trị chiều cao
-var currentHeight = getHeader.clientHeight
+let currentHeight = getHeader.clientHeight
 // click menu
 menuButton.addEventListener('click',function(){
     // Chiều cao header ban đầu là 47 nên thẻ sẽ đóng
-    var currentClose = getHeader.clientHeight === currentHeight
+    let currentClose = getHeader.clientHeight === currentHeight
     if(currentClose){
         getHeader.style.height = 'auto'
     }
@@ -53,14 +54,14 @@ menuButton.addEventListener('click',function(){
     
 })
 
-// sau khi click vào menu sẽ ẩn menu đi 
+// sau khi click vào menu sẽ ẩn menu đi ư
 // lấy cấc thẻ a
-var getItems = document.querySelectorAll('#nav li a[href *= "#"]')
+let getItems = document.querySelectorAll('#nav li a[href *= "#"]')
 
 // tạo vòng lặp bắt sự kiện khi nhấn vào từng thẻ a
-for (var Item of getItems){
+for (let Item of getItems){
     Item.addEventListener('click',function(e){
-        var getMenuMore = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav') // kiểm tra có chứa lớp subnav không
+        let getMenuMore = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav') // kiểm tra có chứa lớp subnav không
         if(getMenuMore){
             e.preventDefault()
         }
@@ -72,3 +73,117 @@ for (var Item of getItems){
 
 
 // lấy selector More
+this
+
+
+
+// lấy giá trị người dùng input
+// console.log(personInput.values)
+
+
+// get button submit
+// let btnSubmit = document.querySelector('.btn-submit')
+// // click button
+// btnSubmit.addEventListener('click',function(e){
+//     let personInputs = document.querySelectorAll('.form-control')
+//     for(let personInput of personInputs ){
+//         e.preventDefault()
+//         // btnSubmit.values
+//         console.log(personInput.value)
+//         if(personInput[].value)
+//     }
+
+// })
+
+
+// thực hiên chức năng báo lỗi form
+/**
+ * khi người dùng click vào form-text blur ra ngoài để trống báo lỗi + màu đỏ border
+ * Khi click nút send ô nào còn trống cũng báo lỗi tương tự 
+ * Các bước
+ * 1. Lấy được Element các nút
+ * 2. Kiểm tra value trong nút
+ * 3 Nếu trống thực hiện báo lỗi và ngược lại 
+ *  
+ */
+
+
+
+// lấy element name-input
+let getNameInput = document.querySelector('.input-name')
+//// thưucj hiên hành động blur 
+getNameInput.addEventListener('blur', function getValue(){
+        // kiem tra input có rỗng không
+        if(getNameInput.value.trim() === ''){
+            getNameInput.classList.add('error')
+            spanName.classList.add('error')
+       }
+       else{
+        getNameInput.classList.remove('error')
+       }
+   }
+)
+
+// get EMAIL input
+let getEmailInput = document.querySelector('.input-email')
+getEmailInput.addEventListener('blur', function getValue(){
+    if(getEmailInput.value.trim() === ''){
+        getEmailInput.classList.add('error')
+        spanEmail.classList.add('error')
+
+   }
+   else{
+    getEmailInput.classList.remove('error')
+   }
+})
+
+/// get Message
+let getMessageInput = document.querySelector('.Message-text')
+getMessageInput.addEventListener('blur', function getValue(){
+    if(getMessageInput.value.trim() === ''){
+        getMessageInput.classList.add('error')
+        spanMessage.classList.add('error')
+
+   }
+   else{
+    getMessageInput.classList.remove('error')
+   }
+})
+
+
+// khi không đầy đủ thông tin nhấn subbmit sẽ baó lỗi
+let getBtn_send = document.querySelector('.btn-submit')
+getBtn_send.addEventListener('click', function(e){
+    e.preventDefault()
+    // if(getNameInput.value === '' || getEmailInput.value === '' || getMessageInput.value === '') {
+        if(getNameInput.value.trim() === ''){
+            getNameInput.classList.add('error')
+            spanName.classList.add('error')
+       }
+       else if(getEmailInput.value.trim() === ''){
+            getEmailInput.classList.add('error')
+            spanEmail.classList.add('error')
+       }
+       else if(getMessageInput.value.trim() === ''){
+        getMessageInput.classList.add('error')
+        spanMessage.classList.add('error')
+       }
+       else{
+        getNameInput.classList.add('error')
+        spanName.classList.add('error')
+
+        getEmailInput.classList.add('error')
+        spanEmail.classList.add('error')
+
+        getMessageInput.classList.add('error')
+        spanMessage.classList.add('error')
+       }
+    })
+
+
+
+
+/// get thẻ span báo lỗi
+let spanName = document.querySelector('.error-name')
+let spanEmail = document.querySelector('.error-email')
+let spanMessage = document.querySelector('.error-message')
